79c1f1ad059e33769bdf1f5ea51f9fd8cb827fab
Jonas Koperdraat <jonas@jonaskoperdraat.nl>
2023-09-19 09:18:49 +0200

    Add Kotlin implementation using strategy pattern

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,58 +1,66 @@
 package com.gildedrose
 
+const val AGED_BRIE = "Aged Brie"
+const val BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+const val SULFURAS = "Sulfuras, Hand of Ragnaros"
+
+val udpateStrategies: List<UpdateStrategy> = listOf(
+        AgedBrieStrategy(),
+        BackstagePassesStrategy(),
+        SulfurasStrategy(),
+        ConjuredItemStrategy())
+
 class GildedRose(var items: List<Item>) {
-
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+        for (item in items) {
+            val strategy = udpateStrategies.find { s -> s.appliesTo(item) } ?: GenericStrategy()
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+            val degradation = strategy.getQualityDegradation(item)
+            // This is a bit weird; the quality is only updated, and coerced to be max 50 and min 0,
+            // when there's an actual quality change. There's this requirement that 50 >= quality >= 0,
+            // but at the same time, the validation test initialized a Sulfuras item with
+            // quality 80, which it keeps during the entire duration of the test.
+            if (degradation != 0) {
+                item.quality = (item.quality - degradation).coerceAtLeast(0).coerceAtMost(50)
             }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
+            item.sellIn -= strategy.getSellInDecrement(item)
         }
     }
-
 }
 
+interface UpdateStrategy {
+    fun appliesTo(item: Item): Boolean
+    fun getSellInDecrement(item: Item): Int = 1
+    fun getQualityDegradation(item: Item): Int = 0
+}
+
+class GenericStrategy : UpdateStrategy {
+    override fun appliesTo(item: Item) = true
+    override fun getQualityDegradation(item: Item) = if (item.sellIn > 0) 1 else 2
+}
+
+class AgedBrieStrategy : UpdateStrategy {
+    override fun appliesTo(item: Item) = item.name == AGED_BRIE
+    override fun getQualityDegradation(item: Item) = if (item.sellIn > 0) -1 else -2
+}
+
+class SulfurasStrategy : UpdateStrategy {
+    override fun appliesTo(item: Item) = item.name == SULFURAS
+    override fun getQualityDegradation(item: Item) = 0
+    override fun getSellInDecrement(item: Item) = 0
+}
+
+class BackstagePassesStrategy : UpdateStrategy {
+    override fun appliesTo(item: Item) = item.name == BACKSTAGE_PASSES
+    override fun getQualityDegradation(item: Item) = when {
+        item.sellIn <= 0 -> item.quality
+        item.sellIn <= 5 -> -3
+        item.sellIn <= 10 -> -2
+        else -> -1
+    }
+}
+
+class ConjuredItemStrategy : UpdateStrategy {
+    override fun appliesTo(item: Item) = item.name.lowercase().contains("conjured")
+    override fun getQualityDegradation(item: Item) = if (item.sellIn > 0) 2 else 4
+}
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,7 +1,12 @@
 package com.gildedrose
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 internal class GildedRoseTest {
 
@@ -10,10 +15,181 @@ internal class GildedRoseTest {
         val items = listOf(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+        assertEquals("foo", app.items[0].name)
+    }
 
+    @Test
+    fun regularItemDecreasesInQuality() {
+        // Given
+        val items = listOf(Item("+5 Dexterity Vest", 10, 20))
+
+        // When
+        val app = createAndUpdateApp(items)
+
+        // Then
+        assertThat(app.items[0].name).isEqualTo("+5 Dexterity Vest")
+        assertThat(app.items[0].sellIn).isEqualTo(9)
+        assertThat(app.items[0].quality).isEqualTo(19)
+    }
+
+    @Test
+    fun qualityIsNeverLessThan0() {
+        // Given
+        val items = listOf(Item("Foo", 10, 0))
+
+        // When
+        val app = createAndUpdateApp(items)
+
+        // Then
+        assertThat(app.items[0].quality).isEqualTo(0)
+    }
+
+    @Test
+    fun qualityDegradesTwiceAsFastPastSellDate() {
+        // Given
+        val items = listOf(Item("Foo", 2, 10))
+
+        // When
+        val app = createAndUpdateApp(items, 4)
+
+        // Then
+        // 10 - 1 - 1 - 2 - 2 = 4
+        assertThat(app.items[0].sellIn).isEqualTo(-2)
+        assertThat(app.items[0].quality).isEqualTo(4)
+    }
+
+    @Test
+    fun agedBrieIncreasesInQuality() {
+        // Given
+        val items = listOf(Item(AGED_BRIE, 5, 10))
+
+        // When
+        val app = createAndUpdateApp(items, 3)
+
+        // Then
+        assertThat(app.items[0].quality).isEqualTo(13)
+    }
+
+    @Test
+    fun agedBrieIncreasesTwiceAsFastPastSellDate() {
+        // Given
+        val items = listOf(Item(AGED_BRIE, 2, 10))
+
+        // When
+        val app = createAndUpdateApp(items, 4)
+
+        // Then
+        // 10 + 1 + 1 + 2 + 2 = 16
+        assertThat(app.items[0].quality).isEqualTo(16)
+    }
+
+    @Test
+    fun qualityIsNeverMoreThan50() {
+        // Given
+        val items = listOf(Item(AGED_BRIE, 5, 40))
+
+        // When
+        val app = createAndUpdateApp(items, 15)
+
+        // Then
+        assertThat(app.items[0].quality).isEqualTo(50)
+    }
+
+    @Test
+    fun sulfurasIsStationary() {
+        // Given
+        val items = listOf(Item(SULFURAS, 5, 10))
+
+        // When
+        val app = createAndUpdateApp(items, 20)
+
+        // Then
+        assertThat(app.items[0].sellIn).isEqualTo(5)
+        assertThat(app.items[0].quality).isEqualTo(10)
+    }
+
+    @Test
+    fun initialQualityCanBeOutOfBounds() {
+        // Given
+        val items = listOf(
+                Item(SULFURAS, 5, 80),
+                Item(SULFURAS, 4, -2))
+
+        // When
+        val app = createAndUpdateApp(items, 2)
+
+        // Then
+        assertThat(app.items[0].quality).isEqualTo(80)
+        assertThat(app.items[1].quality).isEqualTo(-2)
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    fun backstagePasses(item: Item, days: Int, expectedSellIn: Int, expectedQuality: Int) {
+        // Given
+        val items = listOf(item)
+        println(item)
+
+        // When
+        val app = createAndUpdateApp(items, days)
+        println(app.items[0])
+
+        // Then
+        assertThat(app.items[0].sellIn).isEqualTo(expectedSellIn)
+        assertThat(app.items[0].quality).isEqualTo(expectedQuality)
+    }
+
+    companion object {
+        @JvmStatic
+        fun backstagePasses(): Stream<Arguments> {
+
+            return Stream.of(
+                    Arguments.of(Item(BACKSTAGE_PASSES, 12, 2), 0, 12, 2),
+                    Arguments.of(Item(BACKSTAGE_PASSES, 12, 2), 1, 11, 3),
+                    Arguments.of(Item(BACKSTAGE_PASSES, 12, 2), 2, 10, 4),
+                    Arguments.of(Item(BACKSTAGE_PASSES, 12, 2), 3,  9, 6),
+                    Arguments.of(Item(BACKSTAGE_PASSES, 12, 2), 4,  8, 8),
+                    Arguments.of(Item(BACKSTAGE_PASSES, 12, 2), 5,  7, 10),
+                    Arguments.of(Item(BACKSTAGE_PASSES, 12, 2), 6,  6, 12),
+                    Arguments.of(Item(BACKSTAGE_PASSES, 12, 2), 7,  5, 14),
+                    Arguments.of(Item(BACKSTAGE_PASSES, 12, 2), 8,  4, 17),
+                    Arguments.of(Item(BACKSTAGE_PASSES, 12, 2), 9,  3, 20),
+                    Arguments.of(Item(BACKSTAGE_PASSES, 12, 2), 10, 2, 23),
+                    Arguments.of(Item(BACKSTAGE_PASSES, 12, 2), 11, 1, 26),
+                    Arguments.of(Item(BACKSTAGE_PASSES, 12, 2), 12, 0, 29),
+                    Arguments.of(Item(BACKSTAGE_PASSES, 12, 2), 13, -1, 0),
+            )
+        }
+    }
+
+    @Test
+    fun conjuredItemDegradesTwiceAsFastAsRegularItem() {
+        // Given
+        val items = listOf(
+                Item("Spoon", 5, 10),
+                Item("Conjured spoon", 5, 10)
+        )
+
+        // When
+        val app = createAndUpdateApp(items, 3)
+
+        // Then
+        val spoon = app.items[0]
+        val conjuredSpoon = app.items[1]
+
+        assertThat(spoon.name).isEqualTo("Spoon")
+        assertThat(spoon.sellIn).isEqualTo(2)
+        assertThat(spoon.quality).isEqualTo(7)
+        assertThat(conjuredSpoon.name).isEqualTo("Conjured spoon")
+        assertThat(conjuredSpoon.sellIn).isEqualTo(2)
+        assertThat(conjuredSpoon.quality).isEqualTo(4)
+    }
+
+    private fun createAndUpdateApp(items: List<Item>, times: Int = 1): GildedRose {
+        val app = GildedRose(items)
+        repeat(times) { _ ->  app.updateQuality() }
+
+        return app
     }
 
 }
-
-
